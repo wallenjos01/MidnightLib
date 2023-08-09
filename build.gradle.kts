466c8375ce19986ff947f42ab5f0af484e4e0c3e
M1dnightNinja <wallenjos01@gmail.com>
2023-08-09 13:54:12 -0600

    Bump snapshot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "org.wallentines"
-version = "1.2.2-SNAPSHOT"
+version = "1.3.0-SNAPSHOT"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
