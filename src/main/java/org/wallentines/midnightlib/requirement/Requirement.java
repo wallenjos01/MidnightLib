ccef0fe7066292428d455f9461eb77c214d7e61b
wallenjos01 <wallenjos01@gmail.com>
2024-12-20 12:07:58 -0700

    Add requirement.serializer() with custom constructor

--- src/main/java/org/wallentines/midnightlib/requirement/Requirement.java
+++ src/main/java/org/wallentines/midnightlib/requirement/Requirement.java
@@ -4,6 +4,8 @@ import org.wallentines.mdcfg.serializer.ObjectSerializer;
 import org.wallentines.mdcfg.serializer.Serializer;
 import org.wallentines.midnightlib.registry.Registry;
 
+import java.util.function.BiFunction;
+
 public class Requirement<T> {
 
     protected final Check<T> check;
@@ -23,9 +25,14 @@ public class Requirement<T> {
     }
 
     public static <T> Serializer<Requirement<T>> serializer(Registry<?, CheckType<T, ?>> registry) {
+        return serializer(registry, Requirement::new);
+    }
+
+    public static <T, R extends Requirement<T>> Serializer<R> serializer(Registry<?, CheckType<T, ?>> registry, BiFunction<Check<T>, Boolean, R> constructor) {
         return Check.serializer(registry).dispatch(check ->
-                ObjectSerializer.create(Serializer.BOOLEAN.<Requirement<T>>entry("invert", Requirement::isInverted).orElse(false),
-                invert -> new Requirement<>(check, invert)),
+                        ObjectSerializer.create(Serializer.BOOLEAN.entry("invert", R::isInverted).orElse(false),
+                                invert -> constructor.apply(check, invert)),
                 req -> req.check);
     }
+
 }
